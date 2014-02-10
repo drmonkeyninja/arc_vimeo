b133524714160ded32ec9ac84cc8d2bc173ce5cf
Andy Carter <andy1704@gmail.com>
2014-02-10 20:43:15 +0000

    Added help documentation.

--- arc_vimeo.php
+++ arc_vimeo.php
@@ -10,10 +10,64 @@ $plugin['type'] = 0;
 @include_once('zem_tpl.php');
 
 if (0) {
+	?>
 # --- BEGIN PLUGIN HELP ---
 
+h1. arc_vimeo
+
+h2. Description
+
+Easily embed Vimeo videos in articles and customise the appearance of the player.
+
+
+h2. Installation
+
+To install go to the 'plugins' tab under 'admin' and paste the plugin code into the 'Install plugin' box, 'upload' and then 'install'. Please note that you will need to set-up a custom field to use for associating videos with articles, unless you choose to directly embed the new tag in the article text.
+
+
+h2. Syntax
+
+bc.. <txp:arc_vimeo />
+
+<txp:arc_vimeo video='86295452' width='500' ratio='16:9' />
+
+<txp:arc_vimeo video='http://vimeo.com/86295452' width='500' ratio='16:9' />
+
+h2. Usage
+
+h3. Video
+
+|_. Attribute|_. Description|_. Default|_. Example|
+|video|Vimeo url or video ID for the video you want to embed| _unset_|video='86295452'|
+|custom|Name of the custom field containing video IDs/urls associated with article|Vimeo ID|custom='video'|
+
+h3. Basics
+
+|_. Attribute|_. Description|_. Default|_. Example|
+|label|Label for the video| _no label_|label='Vimeo video'|
+|labeltag|Independent wraptag for label| _empty_|labeltag='h3'|
+|wraptag|HTML tag to be used as the wraptag, without brackets| _unset_|wraptag='div'|
+|class|CSS class attribute for wraptag|arc_vimeo|class='vimeo'|
+
+h3. Customising the Vimeo player
+
+You can customise the appearance of the Vimeo player using this plugin to define things like colours and size.
+
+|_. Attribute|_. Description|_. Default|_. Example|
+|width|Width of video|0|width='200'|
+|height|Height of video|0|height='150'|
+|ratio|Aspect ratio|4:3|ration='16:9'|
+|color|A hex colour code for the player UI elements|00adef|color='ff6500'|
+|portrait|'0' to disable the user's portrait|1| |
+|title|'0' to disable the video's title|1| |
+|byline|'0' to disable the video's byline|1| |
+|badge|'0' to disable the video's badge|1| |
+|loop|'1' to loop the video on play|0| |
+|autoplay|'1' to autoplay the video, '0' to turn off autoplay (default)|0| |
+|autopause|'1' to autopause the video when another is played on the same page|0| |
 
 # --- END PLUGIN HELP ---
+<?php
 }
 
 # --- BEGIN PLUGIN CODE ---
