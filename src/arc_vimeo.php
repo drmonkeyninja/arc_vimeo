25cf794c5ade29d7c802f2651616825a9eca7db1
Andy Carter <andy1704@gmail.com>
2014-10-26 20:27:10 +0000

    Updated version number to v1.1.1

--- src/arc_vimeo.php
+++ src/arc_vimeo.php
@@ -1,7 +1,7 @@
 <?php
 $plugin['name'] = 'arc_vimeo';
 
-$plugin['version'] = '1.1';
+$plugin['version'] = '1.1.1';
 $plugin['author'] = 'Andy Carter';
 $plugin['author_uri'] = 'http://andy-carter.com/';
 $plugin['description'] = 'Embed Vimeo videos with customised player';
